4b9b1f1534e400d0c9567bd5eb989ab55a1b5db5
Sultanxda <sultanxda@gmail.com>
2015-07-14 13:32:03 -0700

    ham: QCamera2: Always keep DIS disabled
    DIS does not work properly, and breaks video recording on apps that decide to enable it.
    
    Change-Id: Ifc414bd27d2582ddc639406a3f13af4bd510dd0c

--- camera/QCamera2/HAL/QCameraParameters.cpp
+++ camera/QCamera2/HAL/QCameraParameters.cpp
@@ -2762,8 +2762,9 @@ int32_t QCameraParameters::setMCEValue(const QCameraParameters& params)
  *              NO_ERROR  -- success
  *              none-zero failure code
  *==========================================================================*/
-int32_t QCameraParameters::setDISValue(const QCameraParameters& params)
+int32_t QCameraParameters::setDISValue(__attribute__((unused)) const QCameraParameters& params)
 {
+#if 0
     const char *str = params.get(KEY_QC_DIS);
     const char *prev_str = get(KEY_QC_DIS);
     if (str != NULL) {
@@ -2772,6 +2773,7 @@ int32_t QCameraParameters::setDISValue(const QCameraParameters& params)
             return setDISValue(str);
         }
     }
+#endif
     return NO_ERROR;
 }
 
@@ -5613,8 +5615,9 @@ int32_t QCameraParameters::setCDSMode(const QCameraParameters& params)
  *              NO_ERROR  -- success
  *              none-zero failure code
  *==========================================================================*/
-int32_t QCameraParameters::setDISValue(const char *disStr)
+int32_t QCameraParameters::setDISValue(__attribute__((unused)) const char *disStr)
 {
+#if 0
     if (disStr != NULL) {
         int32_t value = lookupAttr(ENABLE_DISABLE_MODES_MAP,
                                    sizeof(ENABLE_DISABLE_MODES_MAP)/sizeof(QCameraMap),
@@ -5634,6 +5637,8 @@ int32_t QCameraParameters::setDISValue(const char *disStr)
     }
     ALOGE("Invalid DIS value: %s", (disStr == NULL) ? "NULL" : disStr);
     return BAD_VALUE;
+#endif
+    return NO_ERROR;
 }
 
 /*===========================================================================
