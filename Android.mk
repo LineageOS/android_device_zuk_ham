59807b8733938c9ce032e431a727d1750ed4b32d
luca020400 <luca.stefani.ge1@gmail.com>
2016-05-07 17:18:13 +0200

    ham: Fix WCNSS_qcom_cfg.ini symlink
    * m2n ignores this makefile entry leading to non working wifi
    
    Change-Id: Ifde123cb19027bc972d9dc5c167f38bab37da675

--- Android.mk
+++ Android.mk
@@ -19,10 +19,13 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),ham)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
-# Create a link for the WCNSS config file, which ends up as a writable
-# version in /data/misc/wifi
-$(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
-     ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
-     	    $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
+WCNSS_CONFIG_SYMLINK := $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
+$(WCNSS_CONFIG_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "WCNSS config link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /data/misc/wifi/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_CONFIG_SYMLINK)
 
 endif
