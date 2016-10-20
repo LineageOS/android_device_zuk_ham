25019cab2f01fe6a4648004904eac940c743a65d
Louis Popi <theh2o64@gmail.com>
2016-10-20 19:38:27 +0200

    ham: Add ConfigPanel for button swap
    * Squashed changes from xiaomi_msm8996:
    	ham: ConfigPanel: Fix bootup behavior
    	ham: ConfigPanel: Update colors to match SystemUI
    	ham: ConfigPanel: Remove unused code
    	ham: ConfigPanel: Commonize strings translations
    	ham: ConfigPanel: Use cmsdk FileUtils wherever it suits
    	ham: ConfigPanel: Refactor
    
    Change-Id: Ia66fd3fed7cc1687862ab1d35578b525a422f0bb

---
+++ configpanel/src/com/cyanogenmod/settings/device/Constants.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cyanogenmod.settings.device;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Constants {
+
+    // Swap keys
+    public static final String BUTTON_SWAP_KEY = "button_swap";
+
+    // Swap nodes
+    public static final String BUTTON_SWAP_NODE = "/sys/devices/virtual/touch/tp_dev/key_reverse";
+
+    // Holds <preference_key> -> <proc_node> mapping
+    public static final Map<String, String> sBooleanNodePreferenceMap = new HashMap<>();
+    public static final Map<String, String> sStringNodePreferenceMap = new HashMap<>();
+
+    // Holds <preference_key> -> <default_values> mapping
+    public static final Map<String, Object> sNodeDefaultMap = new HashMap<>();
+
+    // Holds <preference_key> -> <user_set_values> mapping
+    public static final Map<String, Object[]> sNodeUserSetValuesMap = new HashMap<>();
+
+    // Holds <preference_key> -> <dependency_check> mapping
+    public static final Map<String, String[]> sNodeDependencyMap = new HashMap<>();
+
+    public static final String[] sButtonPrefKeys = {
+        BUTTON_SWAP_KEY,
+    };
+
+    static {
+        sBooleanNodePreferenceMap.put(BUTTON_SWAP_KEY, BUTTON_SWAP_NODE);
+        sNodeDefaultMap.put(BUTTON_SWAP_KEY, false);
+    }
+}
