fd69f00765ffd41e5b7342ce0873b7443225967a
Artem Borisov <dedsa2002@gmail.com>
2017-08-26 05:15:46 +0000

    ham: libshims: Bring back atomic calls for mpdecision
    Change-Id: Ifa857868a75fe0414b63ee2b55c290a3e75f3646

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -23,3 +23,13 @@ LOCAL_MODULE := libshims_thermal
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := atomic.cpp
+
+LOCAL_MODULE := libshims_atomic
+LOCAL_MODULE_TAGS := optional
+LOCAL_SHARED_LIBRARIES := liblog
+
+include $(BUILD_SHARED_LIBRARY)
