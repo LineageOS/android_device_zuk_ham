85e873354089cf8d623f764df69c7755ae9e95d8
Artem Borisov <dedsa2002@gmail.com>
2017-08-26 05:15:46 +0000

    ham: libshims: Bring back atomic calls for mpdecision
    Change-Id: Ifa857868a75fe0414b63ee2b55c290a3e75f3646

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,6 +76,10 @@ AUDIO_FEATURE_ENABLED_LOW_LATENCY_CAPTURE := true
 AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
 USE_XML_AUDIO_POLICY_CONF := 1
 
+# Bionic
+TARGET_LD_SHIM_LIBS := \
+    /system/vendor/bin/mpdecision|libshims_atomic.so
+
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_QCOM := true
