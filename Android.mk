3beca7e9b304d3c027dc8f48377040580295aa8f
Bruno Martins <bgcngm@gmail.com>
2017-09-04 00:40:11 +0100

    ham: Move WiFi files to vendor
     * The config is now expected to be placed there.
    
     * Move the rest of it to be coherent.
    
     * Edit: Fix "/" typo
    
    Change-Id: I5ce30e12bda443985c828865c8407adbac0ba44d

--- Android.mk
+++ Android.mk
@@ -19,7 +19,7 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),ham)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
-WCNSS_CONFIG_SYMLINK := $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
+WCNSS_CONFIG_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
 $(WCNSS_CONFIG_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	@echo "WCNSS config link: $@"
 	@mkdir -p $(dir $@)
