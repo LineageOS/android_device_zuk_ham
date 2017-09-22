c5c76f097fe9b43943806a9d56a96424008c318e
Demon000 <demonsingur@gmail.com>
2017-09-23 00:31:27 +0300

    ham: light: Turn into a binderized HAL
    Change-Id: Idd0b51c2dc8ba58d5dd1c520318026d2fda8ecac

--- ham.mk
+++ ham.mk
@@ -149,8 +149,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    lights.msm8974
+    android.hardware.light@2.0-service.ham
 
 # IPC Security config
 PRODUCT_COPY_FILES += \
