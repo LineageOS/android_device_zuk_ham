c5de4463c098d009777af044df63a9ebf9570908
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2017-10-30 23:51:55 +0100

    ham: Clean up OMX media packages
     * qcmediaplayer isn't build anymore
     * libdashplayer is a remainer of qcmediaplayer
     * libdivxdrmdecrypt is also dead
    
    Change-Id: I09960280ba901b174829c2291d3a70e5e699d5b2

--- ham.mk
+++ ham.mk
@@ -160,10 +160,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 # Media
- PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
     libc2dcolorconvert \
-    libdivxdrmdecrypt \
-    libdashplayer \
     libOmxAacEnc \
     libOmxAmrEnc \
     libOmxCore \
@@ -172,8 +170,7 @@ PRODUCT_COPY_FILES += \
     libOmxVdec \
     libOmxVdecHevc \
     libOmxVenc \
-    libstagefrighthw \
-    qcmediaplayer
+    libstagefrighthw
 
 # Power
 PRODUCT_PACKAGES += \
