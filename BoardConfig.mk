c42f4999e9db0f7bccdd1b80dc4782bbac909f94
Bruno Martins <bgcngm@gmail.com>
2017-12-16 22:09:09 +0000

    ham: Add compatibility matrix
    Change-Id: I54085a73e48519f1980066c10b54a8783967d362

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,6 +132,7 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Wifi
 BOARD_HAS_QCOM_WLAN              := true
