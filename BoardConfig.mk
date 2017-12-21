2378a016e6cc1e6aabb419b4654eff425665ede3
Michael Bestas <mkbestas@lineageos.org>
2017-12-21 15:10:03 +0000

    ham: Include legacy qcom sepolicy rules
    Change-Id: Ie6e1145ca3d50dc742288f6bdd8369a0f79cfc38

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -209,7 +209,7 @@ endif
 # SELinux policies
 # qcom sepolicy
 include device/qcom/sepolicy/sepolicy.mk
-
+include device/qcom/sepolicy/legacy-sepolicy.mk
 BOARD_SEPOLICY_DIRS += \
     $(DEVICE_PATH)/sepolicy
 
