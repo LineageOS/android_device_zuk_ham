6c8a97a3fb112c124bc0f20fcf401d4b5291756c
Paul Keith <javelinanddart@gmail.com>
2017-12-30 23:56:33 +0100

    ham: Use QTI HIDL power HAL
     * Let's use the new shiny binderized HAL in hardware/qcom/power
    
    Change-Id: I2abf4ca9b0b5a0cb031f12c400a1159962f5e94c

--- ham.mk
+++ ham.mk
@@ -181,8 +181,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    power.msm8974
+    android.hardware.power@1.0-service-qti
 
 # Recovery
 PRODUCT_PACKAGES += \
