ffd0764b969990ddb55a9a3e0381eac95eecad98
Bruno Martins <bgcngm@gmail.com>
2018-01-05 09:06:08 +0000

    ham: Rework init power script a bit
     * Add missing input boots config.
    
     * Enable Adaptive lmk by default.
    
     * Include some comments along the way.
    
    Change-Id: Iccfe3aeb19dd24c071f4af79db45b15f6523d9a0

--- ham.mk
+++ ham.mk
@@ -286,7 +286,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.data.large_tcp_window_size=true \
     telephony.lteOnCdmaDevice=1 \
     wifi.interface=wlan0 \
-    ro.qualcomm.perf.cores_online=2 \
     ro.vendor.extension_library=libqti-perfd-client.so \
     ro.telephony.call_ring.multiple=0
 
