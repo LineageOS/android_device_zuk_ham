d5c4a1115a030e445a0912c975f6601ccbe5bb54
Michael Bestas <mkbestas@lineageos.org>
2018-01-17 07:55:23 +0000

    ham: Move sec_config to vendor
    Change-Id: I0525cf13922f2bfd4df541a71deab13ad842a34b

--- ham.mk
+++ ham.mk
@@ -154,7 +154,7 @@ PRODUCT_PACKAGES += \
 
 # IPC Security config
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+    $(LOCAL_PATH)/configs/sec_config:system/vendor/etc/sec_config
 
 # Media profile
 PRODUCT_COPY_FILES += \
