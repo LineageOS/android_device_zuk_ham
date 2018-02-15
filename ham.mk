d8fa5f5818cd253b85faf21248a3a3638937e4ff
Matthew Bouyack <mbouyack@google.com>
2018-02-15 03:45:17 +0000

    ham: Set the 'latch_unsignaled' property of SF
    Without this change we sometimes skip the execution of SurfaceFlinger
    for one frame, which increases latency and often leads to jank.
    
    This is one of several jank improvements suggested by Qualcomm.
    
    Bug: 68943050
    Change-Id: I3db1519f8cab496454d900b18f0fa1629c33605c

--- ham.mk
+++ ham.mk
@@ -146,8 +146,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwui.use_buffer_age=false \
     debug.sf.recomputecrop=0 \
-    debug.sf.disable_backpressure=1
-
+    debug.sf.disable_backpressure=1 \
+    debug.sf.latch_unsignaled=1
 
 # GPS
 PRODUCT_PACKAGES += \
