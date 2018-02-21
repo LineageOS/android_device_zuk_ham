0ab0a6dbd4f70217fbf1860bdf6e7e1f2ed0e020
Anton Hansson <hansson@google.com>
2018-02-21 06:36:50 +0000

    ham: configpanel: Set LOCAL_SDK_VERSION to current
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Bug: 73535841
    Change-Id: Id5b7526dee882958ee6562cda19ef702b26144f9

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PACKAGE_NAME := ConfigPanel
 
 LOCAL_USE_AAPT2 :=true
