b7c688a17dd59eab1b3250f9c2a67cd68ef2bcb7
sb6596 <shubhamprince111@gmail.com>
2018-09-15 17:04:21 +0000

    ham: Set INITIAL_COPYRIGHT_YEAR
    Change-Id: Ieaef2306ce88207a4547e5ae30dce94d080239b2

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -22,6 +22,8 @@ set -e
 DEVICE=ham
 VENDOR=zuk
 
+INITIAL_COPYRIGHT_YEAR=2015
+
 # Load extractutils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
