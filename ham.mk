043c72642f85263d7d51e063a2edbfd36f25d492
Vladimir Oltean <olteanv@gmail.com>
2018-09-19 06:45:34 +0000

    ham: Set bluetooth address using btnvtool on init
    * And therefore avoids the 'No Bluetooth Address!' fatal error from
      VendorInterface::Open (prior to opening libbt-vendor.so) from
      the bluetooth@1.0 HIDL HAL.
    
    Change-Id: I8044663a350f8f4824074602cb244aabd5a9e497

--- ham.mk
+++ ham.mk
@@ -35,7 +35,8 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.power.rc \
     init.qcom.usb.rc \
-    init.recovery.qcom.rc
+    init.recovery.qcom.rc \
+    init.program_bdaddr.sh
 
 # Emergenry number list
 PRODUCT_PACKAGES += \
