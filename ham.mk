b5945ba39ef7a10c59fcf63aae84ac3af8df1639
guaiyihu <guaiyihu@foxmail.com>
2018-09-20 07:26:34 +0000

    ham: device: Remove non-existent audio.effect@2.0-service
    Change-Id: I3e59bdf98d271cef8218ae0343c3487196486ca4

--- ham.mk
+++ ham.mk
@@ -65,7 +65,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
     audio.a2dp.default \
     audio_amplifier.msm8974 \
     audio_policy.msm8974 \
