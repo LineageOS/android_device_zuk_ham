3d45e0a3320858a3fda095150fcaa62b8622d980
sb6596 <shubhamprince111@gmail.com>
2018-09-28 06:38:27 +0000

    [DNM] ham: Build permissive
    Change-Id: I87dd948bfeb28799c47ce03615a40cffd17c2b47

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,6 +56,7 @@ BOARD_DTBTOOL_ARGS := -2
 TARGET_KERNEL_ARCH := arm
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_CMDLINE := console=tty60,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3b7 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 vmalloc=480M
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8974
 TARGET_KERNEL_CONFIG := lineageos_k9_defconfig
 
