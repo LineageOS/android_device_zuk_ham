3102b0525b6723bc7ccccf327ec79935b506a897
Bruno Martins <bgcngm@gmail.com>
2018-10-29 16:38:41 +0000

    ham: amplifier: Use generated kernel headers
    Change-Id: If70a04e37db545b838a04e42dbb9f30a469c9f82

--- libaudioamp/Android.mk
+++ libaudioamp/Android.mk
@@ -3,12 +3,10 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES += \
-     $(call project-path-for,qcom-audio)/hal/msm8974/ \
-     $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+     $(call project-path-for,qcom-audio)/hal/msm8974/
 
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 LOCAL_HEADER_LIBRARIES += libhardware_headers
-
-LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_SHARED_LIBRARIES := liblog libutils libcutils libdl
 
