ea40d917a1a396811050fb082e766f539fd8e9de
Bruno Martins <bgcngm@gmail.com>
2018-10-29 06:19:23 +0000

    ham: camera: Use generated kernel headers
    Change-Id: Iffb1eebbb32ed8a232067af6300678816e8d03b0

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -38,9 +38,8 @@ LOCAL_C_INCLUDES := \
     $(call project-path-for,qcom-media)/mm-core/inc \
     system/media/camera/include
 
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
+LOCAL_HEADER_LIBRARIES += media_plugin_headers
 
 ifneq ($(call is-platform-sdk-version-at-least,17),true)
   LOCAL_CFLAGS += -include bionic/libc/kernel/common/linux/socket.h
