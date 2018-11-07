9e7db76677e947bb4e337dba7e8fcbe268ae2e32
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    ham: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- ham.mk
+++ ham.mk
@@ -19,8 +19,7 @@
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay vendor/extra/overlays/phone-1080p
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage 
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
