e17f0644aa3b21ec8419a82a9953976838a4a33b
Nicholas Lim <nicknitewolf@lineageos.org>
2019-07-26 20:46:48 +0800

    ham: Add qcom privapp permissions
     * https://source.android.com/devices/tech/config/perms-whitelist#generating-whitelists
    
    Change-Id: Ia90d0bf3a9c2163f2bbd9202384d38f05b442e4f

--- ham.mk
+++ ham.mk
@@ -340,6 +340,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml
 
+# Privapp Whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
