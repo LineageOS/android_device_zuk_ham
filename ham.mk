3163872da5f5cd36fa0f643a6386dde87aedb7d9
sb6596 <shubhamprince111@gmail.com>
2019-10-09 12:07:00 +0000

    ham: Switch to cryptfs_hw hidl service
     * Alos drop deprecated flag 'TARGET_LEGACY_HW_DISK_ENCRYPTION'
    
    Change-Id: Ie924f67f90d64609af3738f20848f08c5c26b781

--- ham.mk
+++ ham.mk
@@ -354,6 +354,10 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+# Legacy Encryption
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
