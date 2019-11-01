85478f46755b40a9b9786463dd66b57bf235db41
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    ham: exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- ham.mk
+++ ham.mk
@@ -20,6 +20,8 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay vendor/extra/overlays/phone-108
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage 
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
